e251d67b8538435855713d7851e2e3e92b9bf26c
Vishal Kumar Garg <vkg5214@gmail.com>
2026-02-22 04:44:28 +0530

    feat: initial Spring Boot project setup with clean architecture

---
+++ src/main/java/com/company/hrms/EnterpriseHrmsApplication.java
@@ -0,0 +1,14 @@
+package com.company.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseHrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseHrmsApplication.class, args);
+        System.out.println("Runnning Application");
+	}
+
+}
